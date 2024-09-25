0759c0101d26575ace6b352f7a97e359dcf92dce
ASalah00 <ahmedeka2000@gmail.com>
2024-09-25 12:56:59 +0300

    modified models attributes and added menu item model and modified tests

--- Littlelemon/restaurant/models.py
+++ Littlelemon/restaurant/models.py
@@ -8,9 +8,17 @@ class Booking(models.Model):
     def __str__(self) -> str:
         return self.name
 class Menu(models.Model):
-    title=models.CharField(max_length=255)
-    price=models.DecimalField(max_digits=10, decimal_places=2)
-    inventory=models.IntegerField()
-    def __str__(self):
-        return f'{self.title} : {str(self.price)}'
+   name = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   menu_item_description = models.TextField(max_length=1000, default='') 
+
+   def __str__(self):
+        return self.name
     
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
